430a43f92261f613bf067c692b114dd1461539ef
Jill Russell <30423289+jillionx@users.noreply.github.com>
2021-09-17 09:51:22 -0400

    Update pe001.cpp

--- project-euler/pe001.cpp
+++ project-euler/pe001.cpp
@@ -12,9 +12,9 @@ int main() {
 
 	for (int i = 0; i < MAX; i++) {
 		if (i % 3 == 0 || i % 5 == 0) { 
-            counter += i;
-        }
-    }
+            		counter += i;
+        	}
+   	}
 
 	cout << counter << endl;
 }
